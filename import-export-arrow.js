2037fa202e50a714560e8f6edca8bb5537af4bb0
Rully Gandadiwirya <rullyrgandadiwirya@gmail.com>
2023-08-02 09:13:01 +0700

    Added jsx

--- import-export-arrow.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Headline from "./headline";
-
-export default () => {
-  const greeting = 'Hello Function Component!';
-
-  return <Headline value={greeting} />;
-};
